ea526c5c4d0c68e38ec21761fcab0c9302600eea
Maxime SERRETTA <maximeserretta+dev@gmail.com>
2024-06-05 17:25:26 +0200

    Modifying path

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,14 @@
+import Logo from "../../assets/icones/logo.png";
 import styles from "./HomePageStyle.module.css";
 
-
-
-export default function HomePage(){
-
-    return(
-        <div className={styles.containerhomepage}>
-            <img className={styles.image}src="/" alt="logo wildygamy"/>
-            <h1 className={styles.title}>Bienvenu dans notre salle d'arcade!</h1>
-            <button type="button" className={styles.button} >Entrer</button>
-        </div>
-    )
+export default function HomePage() {
+  return (
+    <div className={styles.containerhomepage}>
+      <img className={styles.image} src={Logo} alt="logo wildygamy" />
+      <h1 className={styles.title}>Bienvenu dans notre salle d'arcade!</h1>
+      <button type="button" className={styles.button}>
+        Entrer
+      </button>
+    </div>
+  );
 }
-
