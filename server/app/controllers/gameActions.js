774ffab9fc1399cbd83208d07a82bff560a18e6c
Maxime SERRETTA <maximeserretta+dev@gmail.com>
2024-06-05 15:27:14 +0200

    BDD creation rdy to call by id

--- server/app/controllers/gameActions.js
+++ server/app/controllers/gameActions.js
@@ -6,16 +6,16 @@ const browse = async (req, res) => {
   res.json(gamesfromDB);
 };
 
-const read = (req, res) => {
+const read = async (req, res) => {
   const parsedId = parseInt(req.params.id, 10);
 
-  const game = tables.game.find((g) => g.id === parsedId);
+  const game = await tables.game.read(parsedId);
 
   if (game != null) {
     res.json(game);
   } else {
     res.sendStatus(404);
   }
-};
+};  
 
 module.exports = { browse, read };
