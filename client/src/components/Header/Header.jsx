5368dc3dec0ac51dc6e5fd479ae7ece0681b8fd1
Maxime SERRETTA <maximeserretta+dev@gmail.com>
2024-06-07 10:52:39 +0200

    Fixed typo

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-import logo from "../../assets/icones/logo.png"
+import Logo from "../../assets/icones/logo.png"
 import profil from "../../assets/icones/profil.png"
 import styles from "./Header.module.css";
 
 export default function Header(){
     return (
       <div className={styles.containerheader}>
-        <img src={logo} alt="" className={styles.logo} />
+        <img src={Logo} alt="" className={styles.logo} />
         <ul className={styles.list}>
           <li className={styles.list_lots}>
             <Link className={styles.link} to="/">
