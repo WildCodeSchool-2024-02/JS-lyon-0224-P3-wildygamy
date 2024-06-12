e7f026cc8146cf03188b82fc6f2441435877e81e
romain_delol <delol.romain@gmail.com>
2024-06-12 10:34:28 +0200

    only_popular_games_are_shown_on_page

--- client/src/components/CarrousselGames/CarrousselGames.jsx
+++ client/src/components/CarrousselGames/CarrousselGames.jsx
@@ -1,15 +1,23 @@
 import PropTypes from "prop-types";
 import styles from "./CarrousselGames.module.css";
 
-export default function CarrousselJeux({games}) {
+export default function CarrousselJeux({ games }) {
+  const popularGames = games.filter((game) => game.is_popular);
+
   return (
     <>
       <h2>Nos Jeux</h2>
       <div className={styles.carroussel}>
-        {games.map((game) => (
-           <li className={styles.gameItem} key={game.id}>
-            <img className={styles.imageGames} alt={game.name} src={game.image}/>
-            <button type="button" className={styles.buttonGames}>Découvrir</button>
+        {popularGames.map((game) => (
+          <li className={styles.gameItem} key={game.id}>
+            <img
+              className={styles.imageGames}
+              alt={game.name}
+              src={game.image}
+            />
+            <button type="button" className={styles.buttonGames}>
+              Découvrir
+            </button>
           </li>
         ))}
       </div>
@@ -23,6 +31,7 @@ CarrousselJeux.propTypes = {
       id: PropTypes.number.isRequired,
       name: PropTypes.string.isRequired,
       image: PropTypes.string.isRequired,
+      is_popular: PropTypes.bool.isRequired,
     }).isRequired
-  ).isRequired
+  ).isRequired,
 };
