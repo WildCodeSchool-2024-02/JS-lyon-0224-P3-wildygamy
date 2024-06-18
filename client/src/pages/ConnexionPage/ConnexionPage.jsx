10ea222daf5c4d0c06a309697829d49cdd6575e0
lorra7 <laura.baratier@gmail.com>
2024-06-18 16:39:57 +0200

    create file ConnexionPage

---
+++ client/src/pages/ConnexionPage/ConnexionPage.jsx
@@ -0,0 +1,12 @@
+import Logo from "../../assets/icones/logo.png";
+
+import styles from "./ConnexionPage.module.css";
+
+export default function ConnexionPage() {
+  return (
+    <>
+      <img className={styles.logo_connexion} src={Logo} alt="logo wildy gamy" />
+      /
+    </>
+  );
+}
