cb4e6c3aa3c1a70475eff5d943cb38fe728d577f
Salma DESMAZIERES <s.desmazieres@yahoo.fr>
2024-06-19 21:54:46 +0200

    finished css

--- client/src/pages/PrizePage/PrizePage.jsx
+++ client/src/pages/PrizePage/PrizePage.jsx
@@ -6,10 +6,10 @@ export default function PrizePage() {
   const prizes = useLoaderData();
   const littlePrize = prizes.filter((prize) => prize.points < 400);
   const mediumPrize = prizes.filter(
-    (prize) => prize.points > 400 && prize.points < 1000
+    (prize) => prize.points > 400 && prize.points < 900
   );
   const bigPrize = prizes.filter(
-    (prize) => prize.points > 1000 && prize.points < 3000
+    (prize) => prize.points > 900 && prize.points < 3000
   );
   const amazingPrize = prizes.filter((prize) => prize.points > 3000);
 
@@ -17,51 +17,67 @@ export default function PrizePage() {
     <div className={styles.containerPrize}>
       <div className={styles.sectionPrize}>
         <h2>Lots: de 100 points à 300 points </h2>
-        {littlePrize.map((prize) => (
-          <li className={styles.PrizeItem} key={prize.id}>
-            <img
-              className={styles.imagePrize}
-              alt={prize.name}
-              src={prize.image}
-            />
-          </li>
-        ))}
+        <div className={styles.listPrize}>
+          {littlePrize.map((prize) => (
+            <li className={styles.PrizeItem} key={prize.id}>
+              <img
+                className={styles.imagePrize}
+                alt={prize.name}
+                src={prize.image}
+              />
+              <p className={styles.PrizeName}>{prize.name}</p>
+              <p className={styles.PrizePoints}>{prize.points}</p>
+            </li>
+          ))}
+        </div>
       </div>
       <div className={styles.sectionPrize}>
         <h2>Lots: de 400 points à 1000 points </h2>
-        {mediumPrize.map((prize) => (
-          <li className={styles.PrizeItem} key={prize.id}>
-            <img
-              className={styles.imagePrize}
-              alt={prize.name}
-              src={prize.image}
-            />
-          </li>
-        ))}
+        <div className={styles.listPrize}>
+          {mediumPrize.map((prize) => (
+            <li className={styles.PrizeItem} key={prize.id}>
+              <img
+                className={styles.imagePrize}
+                alt={prize.name}
+                src={prize.image}
+              />
+              <p className={styles.PrizeName}>{prize.name}</p>
+              <p className={styles.PrizePoints}>{prize.points}</p>
+            </li>
+          ))}
+        </div>
       </div>
       <div className={styles.sectionPrize}>
         <h2>Lots: de 1000 points à 3000 points </h2>
-        {bigPrize.map((prize) => (
-          <li className={styles.PrizeItem} key={prize.id}>
-            <img
-              className={styles.imagePrize}
-              alt={prize.name}
-              src={prize.image}
-            />
-          </li>
-        ))}
+        <div className={styles.listPrize}>
+          {bigPrize.map((prize) => (
+            <li className={styles.PrizeItem} key={prize.id}>
+              <img
+                className={styles.imagePrize}
+                alt={prize.name}
+                src={prize.image}
+              />
+              <p className={styles.PrizeName}>{prize.name}</p>
+              <p className={styles.PrizePoints}>{prize.points}</p>
+            </li>
+          ))}
+        </div>
       </div>
       <div className={styles.sectionPrize}>
         <h2>Le lot exceptionnel </h2>
-        {amazingPrize.map((prize) => (
-          <li className={styles.PrizeItem} key={prize.id}>
-            <img
-              className={styles.imagePrize}
-              alt={prize.name}
-              src={prize.image}
-            />
-          </li>
-        ))}
+        <div className={styles.listPrize}>
+          {amazingPrize.map((prize) => (
+            <li className={styles.PrizeItem} key={prize.id}>
+              <img
+                className={styles.imagePrize}
+                alt={prize.name}
+                src={prize.image}
+              />
+              <p className={styles.PrizeName}>{prize.name}</p>
+              <p className={styles.PrizePoints}>{prize.points}</p>
+            </li>
+          ))}
+        </div>
       </div>
     </div>
   );
