979e19293b3022d0d0c6a75acc9d426dfa7f912f
romain_delol <delol.romain@gmail.com>
2024-06-19 11:11:24 +0200

    fixed css checkbox

--- client/src/pages/FormAdminGames/FormAdminGames.jsx
+++ client/src/pages/FormAdminGames/FormAdminGames.jsx
@@ -10,16 +10,26 @@ function FormAdminGames() {
       <form method="post" onSubmit={action}>
         <label htmlFor="GameName">Nom du jeu</label>
         <input type="text" id="GameName" name="GameName" />
+
         <label htmlFor="CategoryName">Catégorie</label>
         <input type="text" id="CategoryName" name="CategoryName" />
-        <label htmlFor="ChallengeName">is_challenge</label>
-        <input type="checkbox" id="ChallengeName" name="ChallengeName" />
-        <label htmlFor="PopularName">is_popular</label>
-        <input type="checkbox" id="PopularName" name="PopularName" />
+
+        <div className={styles["checkbox-container"]}>
+          <label htmlFor="ChallengeName">is_challenge</label>
+          <input type="checkbox" id="ChallengeName" name="ChallengeName" />
+        </div>
+
+        <div className={styles["checkbox-container"]}>
+          <label htmlFor="PopularName">is_popular</label>
+          <input type="checkbox" id="PopularName" name="PopularName" />
+        </div>
+
         <label htmlFor="ImageName">Image</label>
         <input type="text" id="ImageName" name="ImageName" />
+
         <label htmlFor="SynopsisName">Synopsis</label>
         <input type="text" id="SynopsisName" name="SynopsisName" />
+
         <button type="submit">Ajouter</button>
       </form>
     </div>
