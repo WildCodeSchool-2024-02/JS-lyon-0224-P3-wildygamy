33805fccedc1185a7633504324459b8367b8ce09
Maxime SERRETTA <maximeserretta+dev@gmail.com>
2024-06-19 15:42:33 +0200

    Prettier fix

--- client/src/pages/DetailPage/DetailPage.jsx
+++ client/src/pages/DetailPage/DetailPage.jsx
@@ -1,18 +1,23 @@
-import { useLoaderData, useParams } from "react-router-dom"
+import { useLoaderData, useParams } from "react-router-dom";
 
-import styles from "./DetailPage.module.css"
+import styles from "./DetailPage.module.css";
 
+export default function DetailPage() {
+  const games = useLoaderData();
+  const { id } = useParams();
 
-export default function DetailPage(){
-  const games = useLoaderData()
-  const {id} = useParams()
-  
-  const selectGame = games.filter((game) => parseInt(game.id, 10) === parseInt(id , 10))
+  const selectGame = games.filter(
+    (game) => parseInt(game.id, 10) === parseInt(id, 10)
+  );
   return (
     <div className={styles.containerDetail}>
-        <h2>{selectGame[0].name}</h2>
-        <img alt={selectGame[0].name} className={styles.imgDetail} src={selectGame[0].image}/>
-        <p>{selectGame[0].synopsis}</p>
+      <h2>{selectGame[0].name}</h2>
+      <img
+        alt={selectGame[0].name}
+        className={styles.imgDetail}
+        src={selectGame[0].image}
+      />
+      <p>{selectGame[0].synopsis}</p>
     </div>
-  )
+  );
 }
