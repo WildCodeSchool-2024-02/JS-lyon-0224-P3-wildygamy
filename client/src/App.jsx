0c691ebfbd76fa031958be92acc49b88aff8b21a
romain_delol <delol.romain@gmail.com>
2024-06-26 17:18:46 +0200

    US41 finished

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,9 @@ import "./App.css";
 function App() {
   // permet de cacher header & footer pour la page d'accueil seulement grâce au useLocation
   const location = useLocation();
-  const hideHeaderFooter = (["/"].includes(location.pathname) || ["/connexion"].includes(location.pathname));
+  const hideHeaderFooter =
+    ["/"].includes(location.pathname) ||
+    ["/connection"].includes(location.pathname);
   return (
     // Si hideHeaderFooter est false affiche le Header/Footer
 
