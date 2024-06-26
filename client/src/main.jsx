ab6c34ee7c2045f1a04e8f1ce8364d5d54ea65fb
Maxime SERRETTA <maximeserretta+dev@gmail.com>
2024-06-26 10:46:04 +0200

    Fixed the route

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -20,7 +20,7 @@ const ApiUrl = import.meta.env.VITE_API_URL;
 
 const handleSignUp = async ({ formData }) => {
   try {
-    const response = await fetch(`${ApiUrl}/api/users`, {
+    const response = await fetch(`${ApiUrl}/api/user`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
